570ccc78391efae27ce6d319b7a5b815d4128587
Olivier Désiré <olivierdesire@MacBook-Pro-de-Olivier.local>
2023-02-19 14:33:52 +0100

    modification rafraichement page favori

--- src/pages/Favoris.js
+++ src/pages/Favoris.js
@@ -86,6 +86,7 @@ const Favoris = ({ baseURL, favoris, setFavoris, updateCookie }) => {
                       copyArray.splice(indexFavoris, 1);
                       setFavoris(copyArray);
                       updateCookie(copyArray);
+                      setIsLoading(true);
                     }}
                   >
                     ⭑
@@ -137,6 +138,7 @@ const Favoris = ({ baseURL, favoris, setFavoris, updateCookie }) => {
                       copyArray.splice(indexFavoris, 1);
                       setFavoris(copyArray);
                       updateCookie(copyArray);
+                      setIsLoading(true);
                     }}
                   >
                     ⭑
